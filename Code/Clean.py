ad1a8d67811366a95caec2876066286ffc0a5822
Samuel Roca <samuel.samuel.roca3@gmail.com>
2020-10-06 17:42:22 +0200

    index condicional

--- Code/Clean.py
+++ Code/Clean.py
@@ -1,22 +1,20 @@
+class Novels:
+	characters = set()
 
-class Clean_novels:
-    characters = set()
+	def __init__(self, text):
+		self.text = text
 
-    def __init__(self, text):
-        self.text = text
-
-    def clean(self):
-        self.text = self.text.replace('_', '')
-
-class Clean_news:
-
-    def __init__(self, text):
-        self.text = text
+	def clean(self):
+		self.text = self.text.replace('_', '')
 
 
+class News:
 
-class Clean_axiv:
+	def __init__(self, text):
+		self.text = text
 
 
-    def __init__(self, text):
-        self.text = text
+class Arxiv:
+
+	def __init__(self, text):
+		self.text = text

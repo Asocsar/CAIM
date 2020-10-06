0e806a1037fd3402cc323ff662a1e9b62f1b9ca9
Asocsar <danielllisto@gmail.com>
2020-10-06 23:49:55 +0200

    Clean Novels

--- Code/Clean.py
+++ Code/Clean.py
@@ -1,4 +1,6 @@
 
+import re 
+
 class Clean_novels:
     characters = set()
 
@@ -10,11 +12,20 @@ class Clean_novels:
 
         texto_entrada = texto_entrada.replace('_', '')
         texto_entrada = texto_entrada.replace('Ô', '')
-
+        texto_entrada = texto_entrada.replace("n't", '')
+        texto_entrada = texto_entrada.replace("'s", '')
+        texto_entrada = re.sub("^\d+\s|\s\d+\s|\s\d+$", "", texto_entrada)
         texto_splited = texto_entrada.split(' ')
-        texto_splited = list(filter(lambda x: len(x) > 1 or x.upper() == 'I', texto_splited))
-        texto_splited = ' '.join(filter(lambda x: len(x) == len([s for s in x if s.isalpha()]) , texto_splited))
+        texto_splited = list(filter(lambda x: len(list(x)) > 1 or x.upper() == 'I', texto_splited))
+
+        texto_splited = list(filter(lambda x: len(x) == len([s for s in x if s.isalpha()]) , texto_splited))
         
+
+
+        texto_splited = ' '.join(list(texto_splited))
+
+        if "mustn't" in texto_splited:
+            print(texto_splited)
 
 
         return texto_splited

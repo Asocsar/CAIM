73c6b672f95c016458841ad5c6e3ef8cd6ce9540
Asocsar <danielllisto@gmail.com>
2020-10-08 00:04:02 +0200

    graphic zips

--- Code/graphic.py
+++ Code/graphic.py
@@ -4,8 +4,18 @@ def show(datos):
     frec = []
     for (rank, (word,frecu)) in enumerate(datos[-3::-1]):
         frec.append(frecu)
-    rango = list(range(0, len(frec)))
-    plt.plot(rango, frec)
+    rango = list(range(1, len(frec)+1))
+    plt.plot(rango, frec, label="Data")
     plt.yscale("log")
     plt.xscale("log")
+
+    c = 10000
+    rank = rango[:]
+    b = 1
+    a = 30
+
+    plt.plot(rank, [c/((x+b)**a) for x in rank], label= "Zips")
+    plt.legend()
+
+
     plt.show()

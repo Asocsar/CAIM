f621f524219a954478d2c812dd4ea121879c6ad1
Samuel Roca <samuel.samuel.roca3@gmail.com>
2020-10-07 12:20:58 +0200

    afinando

--- Code/Clean.py
+++ Code/Clean.py
@@ -14,13 +14,24 @@ def noDigits(word: str) -> bool:
 			return False
 	return True
 
+def noRepeats(word: str) -> bool
+	return True
+
 class News:
 
 	def __init__(self, text: str):
 		self.text = text
 
 	def clean(self) -> str:
-		filteredText = filter(noDigits, self.text.split())
+
+		filteredText = self.text.replace('_', ' ')
+		filteredText = filteredText.replace(':', '')
+		filteredText = filteredText.replace("n't", '')
+		filteredText = filteredText.replace("'", ' ')
+		filteredText = filteredText.replace('.', ' ').split()
+		filteredText = filter(lambda word: len(word) > 1 or word == "a" or word == "i" or word == "A" or word == "I")
+		filteredText = filter(noDigits, filteredText)
+		filteredText = filter(noRepeats, filteredText)
 		self.text = ' '.join(filteredText)
 		return self.text
 

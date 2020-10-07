a61d5f3584081f89bcd47eb9a485a57d4a487133
Samuel Roca <samuel.samuel.roca3@gmail.com>
2020-10-07 23:46:19 +0200

    ...

--- Code/Clean.py
+++ Code/Clean.py
@@ -1,5 +1,6 @@
 import re
 
+
 class Novels:
 	characters = set()
 
@@ -7,28 +8,25 @@ class Novels:
 		self.text = text
 
 
-    def clean(self):
-        texto_entrada = self.text
+	def clean(self):
+		texto_entrada = self.text
 
-        texto_entrada = texto_entrada.replace('_', '')
-        texto_entrada = texto_entrada.replace('Ô', '')
-        texto_entrada = texto_entrada.replace("n't", '')
-        texto_entrada = texto_entrada.replace("'s", '')
-        texto_entrada = re.sub("^\d+\s|\s\d+\s|\s\d+$", "", texto_entrada)
-        texto_splited = texto_entrada.split(' ')
-        texto_splited = list(filter(lambda x: len(list(x)) > 1 or x.upper() == 'I', texto_splited))
+		texto_entrada = texto_entrada.replace('Ô', '')
+		texto_entrada = texto_entrada.replace("n't", '')
+		texto_entrada = texto_entrada.replace('_', '')
+		texto_entrada = texto_entrada.replace("'s", '')
+		texto_entrada = re.sub("^\d+\s|\s\d+\s|\s\d+$", "", texto_entrada)
+		texto_splited = texto_entrada.split(' ')
+		texto_splited = list(filter(lambda x: len(list(x)) > 1 or x.upper() == 'I', texto_splited))
 
-        texto_splited = list(filter(lambda x: len(x) == len([s for s in x if s.isalpha()]) , texto_splited))
-        
+		texto_splited = list(filter(lambda x: len(x) == len([s for s in x if s.isalpha()]), texto_splited))
 
+		texto_splited = ' '.join(list(texto_splited))
 
-        texto_splited = ' '.join(list(texto_splited))
+		if "mustn't" in texto_splited:
+			print(texto_splited)
 
-        if "mustn't" in texto_splited:
-            print(texto_splited)
-
-
-        return texto_splited
+		return texto_splited
 
 
 def noDigits(word: str) -> bool:

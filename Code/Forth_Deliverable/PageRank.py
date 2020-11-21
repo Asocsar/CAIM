72618ed9012dfa6feac1294aef422491acbad0e9
Asocsar <danielllisto@gmail.com>
2020-11-21 03:05:30 +0100

    END

--- Code/Forth_Deliverable/PageRank.py
+++ Code/Forth_Deliverable/PageRank.py
@@ -4,17 +4,9 @@ from collections import namedtuple
 import time
 import sys
 import re
+import numpy as np
+from fractions import Fraction
 
-class Edge:
-    def __init__ (self, origin, dest):
-        self.origin = origin 
-        self.dest = [dest]
-        self.weight = 1
-
-    def __repr__(self):
-        return "edge: {0} {1}".format(self.origin, self.weight)
-        
-    ## write rest of code that you need for this class
 
 class Airport:
     def __init__ (self, iden=None, name=None):
@@ -22,15 +14,16 @@ class Airport:
         self.name = name
         self.routes = []
         self.routeHash = dict()
-        self.outweight =  0 
+        self.outweight =  0.0
 
-    def __repr__(self):
-        return f"{self.code}\t{self.pageIndex}\t{self.name}"
+    #def __repr__(self):
+    #    return f"{self.code}\t{self.pageIndex}\t{self.name}"
 
-edgeList = [] # list of Edge
-edgeHash = dict() # hash of edge to ease the match
+
 airportList = [] # list of Airport
 airportHash = dict() # hash key IATA code -> Airport
+
+sink_nodes = 0
 
 def readAirports(fd):
     print("Reading Airport file from {0}".format(fd))
@@ -69,49 +62,80 @@ def readRoutes(fd):
         if not (re.match('[A-Z]{3}', origin) and re.match('[A-Z]{3}', destin)):
             continue
 
-        if origin in edgeHash:
-            edgeHash[origin].weight += 1
-            if not destin in edgeHash[origin].dest:
-                edgeHash[origin].dest.append(destin)
+        airport_orig = None
+        airport_dest = None
+        
+        if origin in airportHash and destin in airportHash:
+            airport_orig = airportHash[origin]
+            airport_dest = airportHash[destin]
+
+            routes = airport_dest.routeHash
+
+            if origin in routes:
+                routes[origin] += 1
+            else:
+                routes[origin] = 1.0
+            
+            airport_orig.outweight += 1
+
         else:
-            E = Edge(origin, destin)
-            edgeHash[origin] = E
-        
-        try:
-            if destin not in airportHash[origin].routeHash:
-                airportHash[origin].routeHash[destin] = 0
-
-            airportHash[origin].routeHash[destin] += 1
-            airportHash[origin].outweight += 1
-
-        except Exception:
             invalid += 1
-            continue
-        
         
         cont += 1
     
     routesTxt.close()
-    print(f"There were {cont} routes with IATA code and {invalid} invalid ammount of routes")
+
+    global sink_nodes
+    sink_nodes = len([x for x in airportList if x.outweight == 0])
+    print(f"There were {cont} routes with IATA code and {invalid} invalid ammount of routes and {sink_nodes} sink_nodes")
+
+
+
 
 def computePageRanks():
-    print('ho')
-    return 'po'
-    # write your code
+    
+    N = len(airportHash)
+    P = {K:1/N for K in airportHash}
+    L = 0.80
 
-def outputPageRanks():
-    print('hy')
-    # write your code
+    stop_condition = False
+    iterations = 0
+
+    nout = L/float(N)*sink_nodes
+    out_prob = 1.0/(N)
+    while not stop_condition:
+        Q = {K:0 for K in airportHash}
+        for name in airportHash:
+            all_destinations = airportHash[name].routeHash
+            suma = sum([(P[ID]*all_destinations[ID])/(airportHash[ID].outweight) for ID in all_destinations])
+            Q[name] = L * suma + (1-L)/N + nout*out_prob
+        
+
+        out_prob = (1-L)/N + nout*out_prob
+        stop_condition = all([abs(X-Y) <= 1*10**(-15) for (X,Y) in zip(list(P.values()), list(Q.values()))])
+        P = Q
+        iterations += 1
+
+    return (P, iterations)
+    
+
+def outputPageRanks(pagerank):
+    
+    for City in pagerank:
+        print("The final score for {} is {}".format(City, pagerank[City]))
+    
+    print("Total probability {}".format( sum(list(pagerank.values()))) )
 
 def main(argv=None):
     readAirports("airports.txt")
     readRoutes("routes.txt")
     time1 = time.time()
-    iterations = computePageRanks()
+    (pagerank, iterations) = computePageRanks()
     time2 = time.time()
-    outputPageRanks()
+    outputPageRanks(pagerank)
     print("#Iterations:", iterations)
     print("Time of computePageRanks():", time2-time1)
+
 
 
 if __name__ == "__main__":

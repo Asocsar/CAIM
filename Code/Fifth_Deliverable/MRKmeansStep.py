a03c6a3a6405f5bc5d25adf9323d5cada8f552ca
Asocsar <danielllisto@gmail.com>
2020-12-03 02:42:15 +0100

    Intial commit

---
+++ Code/Fifth_Deliverable/MRKmeansStep.py
@@ -0,0 +1,128 @@
+"""
+.. module:: MRKmeansDef
+
+MRKmeansDef
+*************
+
+:Description: MRKmeansDef
+
+    
+
+:Authors: bejar
+    
+
+:Version: 
+
+:Created on: 17/07/2017 7:42 
+
+"""
+
+from mrjob.job import MRJob
+from mrjob.step import MRStep
+
+import numpy as np
+import sys
+
+def eprint(*args, **kwargs):
+    print(*args, file=sys.stderr, **kwargs)
+
+
+class MRKmeansStep(MRJob):
+    prototypes = {}
+    prob_current_doc = {}
+
+    def jaccard(self, prot, doc):
+        """
+        Compute here the Jaccard similarity between  a prototype and a document
+        prot should be a list of pairs (word, probability)
+        doc should be a list of words
+        Words must be alphabeticaly ordered
+
+        The result should be always a value in the range [0,1]
+        """
+        cross = 0
+        sqn1 = 0
+        sqn2 = 0
+        for (w1,p), w2 in zip(prot,doc):
+            if w1 == w2:
+                cross += p*self.prob_current_doc[w2]
+                sqn1 += abs(p)**2
+                sqn2 += abs(self.prob_current_doc[w2])**2
+
+        return cross / (sqn1 + sqn2 - cross)
+
+    def configure_args(self):
+        """
+        Additional configuration flag to get the prototypes files
+
+        :return:
+        """
+        super(MRKmeansStep, self).configure_args()
+        self.add_file_arg('--prot')
+
+    def load_data(self):
+        """
+        Loads the current cluster prototypes
+
+        :return:
+        """
+
+        
+        f = open(self.options.prot, 'r', encoding='utf8')
+        for line in f:
+            cluster, words = line.split(':')
+            cp = []
+            for word in words.split():
+                cp.append((word.split('+')[0], float(word.split('+')[1])))
+            self.prototypes[cluster] = cp
+
+    def assign_prototype(self, _, line):
+        """
+        This is the mapper it should compute the closest prototype to a document
+
+        Words should be sorted alphabetically in the prototypes and the documents
+
+        This function has to return at list of pairs (prototype_id, document words)
+
+        You can add also more elements to the value element, for example the document_id
+        """
+
+        # Each line is a string docid:wor1 word2 ... wordn
+        doc, words = line.split(':')
+        lwords = words.split()
+
+        #
+        # Compute map here
+        #
+
+        # Return pair key, value
+        yield 2, 2
+
+    def aggregate_prototype(self, key, values):
+        """
+        input is cluster and all the documents it has assigned
+        Outputs should be at least a pair (cluster, new prototype)
+
+        It should receive a list with all the words of the documents assigned for a cluster
+
+        The value for each word has to be the frequency of the word divided by the number
+        of documents assigned to the cluster
+
+        Words are ordered alphabetically but you will have to use an efficient structure to
+        compute the frequency of each word
+
+        :param key:
+        :param values:
+        :return:
+        """
+
+        yield 2, 2
+
+    def steps(self):
+        return [MRStep(mapper_init=self.load_data, mapper=self.assign_prototype,
+                       reducer=self.aggregate_prototype)
+            ]
+
+
+if __name__ == '__main__':
+    MRKmeansStep.run()
